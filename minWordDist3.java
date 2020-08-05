90edb66796c22f7331b5b719ba6769320e7346be
albertcoder <eralbert9191@gmail.com>
2020-08-05 00:53:56 -0700

    shortest word distance i, ii, iii

--- minWordDist3.java
+++ minWordDist3.java
@@ -2,8 +2,7 @@
 //space complexity O(1)
 
 class Solution {
-    public int shortestDistance(String[] words, String word1, String word2) {
-        if(words.length == 0 || words == null) return -1;
+    public int shortestWordDistance(String[] words, String word1, String word2) {
         int i1 = -1;
         int i2 = -1;
         int min = Integer.MAX_VALUE;
@@ -12,10 +11,13 @@ class Solution {
                 i1 = i;
             }
             if(word2.equals(words[i])){
+                if(i1 == i){
+                    i1 = i2;
+                }
                 i2 = i;
             }
             if(i1 != -1 && i2 != -1){
-                min = Math.min(min, Math.abs(i1-i2));
+                min = Math.min(min, Math.abs(i1 - i2));
             }
         }
         return min;
